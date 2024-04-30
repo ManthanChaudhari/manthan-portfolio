62002051e8b3f93bb8aee2fc29ea2750f2241f0c
Manthan Chaudhari <154885543+ManthanChaudhari@users.noreply.github.com>
2024-04-30 23:13:12 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,7 +8,7 @@ function Projects() {
     <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-4'>
       <Card projectTitle={"Ecom"} link="https://ecom-lake-five.vercel.app" projectImage={ecomProject} description="Ecommerce website which uses DummyJSON API to fetch data, implements an 'add to cart' feature for items, storing selections in localStorage. It also offers a filtering functionality." techstack={"React, Redux, Tailwind Css"}/>
       <Card projectTitle={"Youtube"} link="https://manthanchaudhari.github.io/videoStream/" projectImage={videoStream} description="Video streaming platform which fetches data from an API to display video information on a webpage. Users can add their favourite videos to a list, toggle between dark and light themes for optimal viewing, and utilize a search bar to find their favourite videos." techstack={"HTML5, CSS3,Javascript"}/>
-      <Card projectTitle={"Mela"} link="https://chat-web-app-alpha.vercel.app" projectImage={mela} description="It uses WebSocket for real-time communication and integrates Appwrite for secure authentication. Users can seamlessly join or initiate meetings with friends for interactive collaboration." techstack={"React, Redux, Tailwind Css, Websocket, Appwrite"}/>
+      <Card projectTitle={"Mela"} link="https://github.com/ManthanChaudhari/ChatWebApp" projectImage={mela} description="It uses WebSocket for real-time communication and integrates Appwrite for secure authentication. Users can seamlessly join or initiate meetings with friends for interactive collaboration." techstack={"React, Redux, Tailwind Css, Websocket, Appwrite"}/>
     </div>
   )
 }
